ad5033e8211a82e613b6ab5fe543d67265ca99bd
Leigh MacDonald <leigh.macdonald@gmail.com>
2021-06-08 18:30:42 -0600

    Drop linter config in favour of defaults

--- rcon/rcon_test.go
+++ rcon/rcon_test.go
@@ -20,7 +20,9 @@ func startTestServer(fn func(net.Conn, *bytes.Buffer)) (string, error) {
 		if err != nil {
 			return
 		}
-		defer conn.Close()
+		defer func() {
+			_ = conn.Close()
+		}()
 
 		buf := make([]byte, readBufferSize)
 		_, err = conn.Read(buf)
@@ -31,9 +33,19 @@ func startTestServer(fn func(net.Conn, *bytes.Buffer)) (string, error) {
 		var packetSize, requestId, cmdType int32
 		var str []byte
 		b := bytes.NewBuffer(buf)
-		binary.Read(b, binary.LittleEndian, &packetSize)
-		binary.Read(b, binary.LittleEndian, &requestId)
-		binary.Read(b, binary.LittleEndian, &cmdType)
+		var re error
+		re = binary.Read(b, binary.LittleEndian, &packetSize)
+		if re != nil {
+			return
+		}
+		re = binary.Read(b, binary.LittleEndian, &requestId)
+		if re != nil {
+			return
+		}
+		re = binary.Read(b, binary.LittleEndian, &cmdType)
+		if re != nil {
+			return
+		}
 		str, err = b.ReadBytes(0x00)
 		if err != nil {
 			return
@@ -43,12 +55,30 @@ func startTestServer(fn func(net.Conn, *bytes.Buffer)) (string, error) {
 		}
 
 		b.Reset()
-		binary.Write(b, binary.LittleEndian, int32(10))
-		binary.Write(b, binary.LittleEndian, int32(requestId))
-		binary.Write(b, binary.LittleEndian, int32(respAuthResponse))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		conn.Write(b.Bytes())
+		re = binary.Write(b, binary.LittleEndian, int32(10))
+		if re != nil {
+			return
+		}
+		re = binary.Write(b, binary.LittleEndian, requestId)
+		if re != nil {
+			return
+		}
+		re = binary.Write(b, binary.LittleEndian, int32(respAuthResponse))
+		if re != nil {
+			return
+		}
+		re = binary.Write(b, binary.LittleEndian, byte(0))
+		if re != nil {
+			return
+		}
+		re = binary.Write(b, binary.LittleEndian, byte(0))
+		if re != nil {
+			return
+		}
+		_, re = conn.Write(b.Bytes())
+		if re != nil {
+			return
+		}
 
 		if fn != nil {
 			b.Reset()
@@ -80,59 +110,148 @@ func TestMultipacket(t *testing.T) {
 	addr, err := startTestServer(func(c net.Conn, b *bytes.Buffer) {
 		// start packet
 		// start response
-		binary.Write(b, binary.LittleEndian, int32(10+4000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(respResponse))
-		for i := 0; i < 4000; i += 1 {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+		var we error
+		we = binary.Write(b, binary.LittleEndian, int32(10+4000))
+		if we != nil {
+			return
 		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
+		we = binary.Write(b, binary.LittleEndian, int32(123))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, int32(respResponse))
+		if we != nil {
+			return
+		}
+		for i := 0; i < 4000; i += 1 {
+			we = binary.Write(b, binary.LittleEndian, byte(' '))
+			if we != nil {
+				return
+			}
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
 		// end response
 		// start response
-		binary.Write(b, binary.LittleEndian, int32(10+4000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(respResponse))
-		for i := 0; i < 2000; i += 1 {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+		we = binary.Write(b, binary.LittleEndian, int32(10+4000))
+		if we != nil {
+			return
 		}
-		c.Write(b.Bytes())
+		we = binary.Write(b, binary.LittleEndian, int32(123))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, int32(respResponse))
+		if we != nil {
+			return
+		}
+		for i := 0; i < 2000; i += 1 {
+			we = binary.Write(b, binary.LittleEndian, byte(' '))
+			if we != nil {
+				return
+			}
+		}
+		_, we = c.Write(b.Bytes())
+		if we != nil {
+			return
+		}
 		// end packet
 
 		// start packet
 		b.Reset()
 		for i := 0; i < 2000; i += 1 {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+			we = binary.Write(b, binary.LittleEndian, byte(' '))
+			if we != nil {
+				return
+			}
 		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
 		// end response
 		// start response
-		binary.Write(b, binary.LittleEndian, int32(10+2000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(respResponse))
-		for i := 0; i < 2000; i += 1 {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+		we = binary.Write(b, binary.LittleEndian, int32(10+2000))
+		if we != nil {
+			return
 		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
+		we = binary.Write(b, binary.LittleEndian, int32(123))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, int32(respResponse))
+		if we != nil {
+			return
+		}
+
+		for i := 0; i < 2000; i += 1 {
+			we = binary.Write(b, binary.LittleEndian, byte(' '))
+			if we != nil {
+				return
+			}
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
 		// end response
 		// start response - size word is split!
-		binary.Write(b, binary.LittleEndian, int32(10+2000))
-		c.Write(b.Bytes()[:len(b.Bytes())-3])
+		we = binary.Write(b, binary.LittleEndian, int32(10+2000))
+		if we != nil {
+			return
+		}
+		_, we = c.Write(b.Bytes()[:len(b.Bytes())-3])
+		if we != nil {
+			return
+		}
 		// end packet
 
 		b.Reset()
-		binary.Write(b, binary.LittleEndian, int32(10+2000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(respResponse))
-		for i := 0; i < 2000; i += 1 {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+		we = binary.Write(b, binary.LittleEndian, int32(10+2000))
+		if we != nil {
+			return
 		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
+		we = binary.Write(b, binary.LittleEndian, int32(123))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, int32(respResponse))
+		if we != nil {
+			return
+		}
+		for i := 0; i < 2000; i += 1 {
+			we = binary.Write(b, binary.LittleEndian, byte(' '))
+			if we != nil {
+				return
+			}
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
+		we = binary.Write(b, binary.LittleEndian, byte(0))
+		if we != nil {
+			return
+		}
 		// end response
-		c.Write(b.Bytes()[1:])
+		_, we = c.Write(b.Bytes()[1:])
+		if we != nil {
+			return
+		}
 		// end packet
 	})
 	if err != nil {
